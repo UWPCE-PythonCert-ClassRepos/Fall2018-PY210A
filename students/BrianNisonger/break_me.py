45655070df11a1a4f6e608321234bacaaa0edc34
Brian Nisonger <vanulor@gmail.com>
2018-10-15 20:19:31 -0700

    break_me_assignment

---
+++ students/BrianNisonger/break_me.py
@@ -0,0 +1,18 @@
+def NameError():
+	print (x)
+
+def TypeError():
+	a=5;
+	b="17"
+	return a+b
+#def SyntaxError():
+	#for(i;i++;i=0)
+def AttributeError():
+	x=7
+	x.append(7)
+	
+	
+#NameError()
+#TypeError()
+#SyntaxError()
+AttributeError()
