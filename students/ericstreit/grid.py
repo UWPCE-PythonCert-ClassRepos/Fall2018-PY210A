fe4794646fdff5b35a5f58ae14d3b6d5a0c8f0b8
eric streit <misterio@uw.edu>
2018-10-17 09:20:49 -0700

    updated grid

--- students/ericstreit/grid.py
+++ students/ericstreit/grid.py
@@ -1,20 +1,17 @@
-#scratch pad for class 2
-#
-#if __name__=="__main__":
-#   ....assert testing parameters
-#   print("All assertions passed")
-#
-#
-#assert
-gridsize = int(input("How big of a grid would you like?"))
-print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
-for i in range(gridsize):
-    print("|" + " " *gridsize + "|" + " " *gridsize + "|")
-print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
-for i in range(gridsize):
-    print("|" + " " *gridsize + "|" + " " *gridsize + "|")
-print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
 
+#grid homework
+#updated version
+borderline = "+" + "-" *gridsize + "+" + "-" *gridsize + "+"
+innerline = "|" + " " *gridsize + "|" + " " *gridsize + "|"
+gridsize = int(input("How big of a grid would you like?"))
+print(borderline)
+for i in range(gridsize):
+    print(innerline)
+print(borderline)
+for i in range(gridsize):
+    print(innerline)
+print(borderline)
+#first attempt
 # print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
 # print("|" + " " *gridsize + "|" + " " *gridsize + "|")
 # print("|" + " " *gridsize + "|" + " " *gridsize + "|")
