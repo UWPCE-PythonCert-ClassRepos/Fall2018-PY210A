f03679891c73953b5a7d9661392dc18393ba4799
eric streit <misterio@uw.edu>
2018-10-16 20:54:25 -0700

    grid work

---
+++ students/ericstreit/grid.py
@@ -0,0 +1,29 @@
+#scratch pad for class 2
+#
+#if __name__=="__main__":
+#   ....assert testing parameters
+#   print("All assertions passed")
+#
+#
+#assert
+gridsize = int(input("How big of a grid would you like?"))
+print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
+for i in range(gridsize):
+    print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
+for i in range(gridsize):
+    print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
+
+# print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("|" + " " *gridsize + "|" + " " *gridsize + "|")
+# print("+" + "-" *gridsize + "+" + "-" *gridsize + "+")
+input("press any key to continue")
