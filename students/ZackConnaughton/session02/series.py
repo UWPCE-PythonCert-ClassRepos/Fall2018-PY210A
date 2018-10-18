4851ed8bbf0c40e895acc491385ff0c0c8472fcc
Zack Connaughton <zconn@uw.edu>
2018-10-17 21:22:05 -0700

    Generalized series.py by adding sum_series.py to use the same function with different parameters for fibonacci and lucas series values.

--- students/ZackConnaughton/session02/series.py
+++ students/ZackConnaughton/session02/series.py
@@ -1,32 +1,36 @@
 def fibonacci(n):
     """Returns as an integer the nth value in the fibanacci sequence"""
+    return sum_series(n)
 
-    i = 0
-    j = 1
-    for x in range(n-1):
-        #print(str(i) + " " + str(j))
-        j, i = add_values(i, j), j
-    return i
-
-def add_values(i, j):
-    return i + j
-
-def fibo(n):
-
-    if n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
-        return fibo(n-2) + fibo(n-1)
+    # if n == 1:
+    #     return 0
+    # elif n == 2:
+    #     return 1
+    # else:
+    #     return fibonacci(n-2) + fibonacci(n-1)
 
 def lucas(n):
+    """ doc string"""
+
+    return sum_series(n, n0=2, n1=1)
+    # if n == 0:
+    #     return 2
+    # elif n == 1:
+    #     return 1
+    # else:
+    #     return lucas(n - 2) + lucas (n - 1)
+
+def sum_series(n, n0=0, n1=1):
+    """
+    compute the nth value of a summation series.
+    """
+
     if n == 0:
-        return 2
+        return n0
     elif n == 1:
-        return 1
+        return n1
     else:
-        return lucas(n - 2) + lucas (n - 1)
+        return sum_series(n-2, n0=n0, n1=n1) + sum_series(n-1, n0=n0, n1=n1)
 
 if __name__ == "__main__":
     # run some tests
@@ -43,6 +47,7 @@ if __name__ == "__main__":
     assert lucas(1) == 1
 
     assert lucas(4) == 7
+    assert lucas(8) == 47
 
     assert sum_series(5) == fibonacci(5)
 
