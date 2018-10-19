b8fb6952c62270bb873ebf1a9a5279856b54623f
Dianna Tingg <dtingg@uw.edu>
2018-10-18 17:06:56 -0700

    added part 2

--- students/DiannaTingg/Session02/grid_printer.py
+++ students/DiannaTingg/Session02/grid_printer.py
@@ -1,9 +1,9 @@
 # Lesson 02 Exercise: Grid Printer
 
-# Part 1: Write a function that draws a gird like the example
+# Part 1: Write a function that draws a grid like the example
 
 
-def print_grid():
+def standard_grid():
     # Top row
     print("+ " + "- " * 4 + "+ " + "- " * 4 + "+")
 
@@ -23,4 +23,34 @@ def print_grid():
     # Bottom row
     print("+ " + "- " * 4 + "+ " + "- " * 4 + "+")
 
-print_grid()
+
+# Test
+print("Standard Grid")
+standard_grid()
+
+# Part 2 Write a function that prints the grid size according to an integer
+
+
+def print_grid(n):
+    half = n // 2
+
+    # Top row
+    print("+ " + "- " * half + "+ " + "- " * half + "+")
+
+    for i in range(half):
+        print("| " + " " * (half * 2) + "| " + " " * (half * 2) + "|")
+
+    # Middle row
+    print("+ " + "- " * half + "+ " + "- " * half + "+")
+
+    for i in range(half):
+        print("| " + " " * (half * 2) + "| " + " " * (half * 2) + "|")
+
+    # Bottom row
+    print("+ " + "- " * half + "+ " + "- " * half + "+")
+
+
+# Tests
+print("Scalable Grid")
+print_grid(3)
+print_grid(15)
