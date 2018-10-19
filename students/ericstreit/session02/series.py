6654a71dcd148c5fa0c0f35421c2f9ed9d5f7db2
eric streit <misterio@uw.edu>
2018-10-19 13:57:15 -0700

    updated series homework

--- students/ericstreit/session02/series.py
+++ students/ericstreit/session02/series.py
@@ -30,9 +30,26 @@ def lucas(n):
         b=c
     #print(f'the {i+3}th number in the lucas number sequence is {c}')
     return(c)
+#the sum series function that
+def sum_series(n,n2 = 0,n3 = 1):
+    """This is the sum seires function that will return the nth value of a series of numbers. 1st option returns the nth number and the 2nd and 3rd option is the beginning of the series. Default is 0 and 1 for fibonacci"""
+    a=n2
+    b=n3
+    #comment out all but the last of the print statements if you don't want to see the count increase
+    #print(a)
+    #print(b)
+    for i in range(n-2):
+        c=a+b
+        #print(c)
+        a=b
+        b=c
+    return(c)
+
 #validating functions
 if __name__=="__main__":
     #fibonacci(14)
     assert fibonacci(12) == 89
     assert lucas(12) == 199
+    assert sum_series(8,2,1) == 29
+    assert sum_series(8) == 13
     print("assertions are valid")
