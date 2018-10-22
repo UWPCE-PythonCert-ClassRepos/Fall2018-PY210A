199d135d4c88dc72b60c8bd784db6c0c90f607ca
Thanh Nhan <thanhnhan@thanhs-mbp.lan>
2018-10-21 20:51:57 -0700

    my update homework

--- students/ThanhNhan/lesson02/assignment2/fizzbuzz.py
+++ students/ThanhNhan/lesson02/assignment2/fizzbuzz.py
@@ -1,9 +1,16 @@
 
-x=1
-for x in range (100):
-	if (x % 3):
-		print ("Fizz")
-	elsif (x % 5):
-		print ("Buzz")
-	else
-		print (x)
+def fizbuz():
+	
+	for x in range (1, 101): 
+		if (x % 5 == 0 and x % 3 == 0):
+			print("FizzBuzz")
+		elif (x % 3 == 0):
+			print("Fizz")
+		elif (x % 5 == 0):
+			print("Buzz")
+		else:
+			print(x)
+	#	print (pr)
+
+if __name__ == "__main__":
+	fizbuz()
