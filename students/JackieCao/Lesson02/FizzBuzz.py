4d65d1d10c339a5fb891915d876fa7b59287aa05
Jackie Cao <jackcao@uw.edu>
2018-10-22 21:47:20 -0700

    switched to python version3 and fixed the indent

--- students/JackieCao/Lesson02/FizzBuzz.py
+++ students/JackieCao/Lesson02/FizzBuzz.py
@@ -1,11 +1,10 @@
-
 for i in range(1,101):
     if(i % (3*5) == 0):
-	print("FizzBuzz")
+        print("FizzBuzz")
     elif(i % 3 == 0):
         print("Fizz")
     elif(i % 5 == 0):
-	print("Buzz")
+        print("Buzz")
     else:
         print(i)
 
