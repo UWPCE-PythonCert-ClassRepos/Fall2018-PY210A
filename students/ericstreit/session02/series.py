84d716866a3676ee4a63559f193f676cf3ad300a
eric streit <misterio@uw.edu>
2018-10-22 19:07:45 -0700

    updated series.py file

--- students/ericstreit/session02/series.py
+++ students/ericstreit/session02/series.py
@@ -1,7 +1,7 @@
 
 #fibonacci series exercise
 def fibonacci(n):
-    """This is the Fibonacci function that will return the nth value"""
+    """This is the Fibonacci function that will return the nth value. This series starts with 0 and 1"""
     a=0
     b=1
     #comment out all but the last of the print statements if you don't want to see the count increase
@@ -17,7 +17,7 @@ def fibonacci(n):
 
 #lucas numbers series exercise
 def lucas(n):
-    """This is the Lucas Numbers function that will return the nth value"""
+    """This is the Lucas Numbers function that will return the nth value. This series starts with 2 and 1"""
     a=2
     b=1
     #comment out all but the last of the print statements if you don't want to see the count increase
@@ -32,7 +32,7 @@ def lucas(n):
     return(c)
 #the sum series function that
 def sum_series(n,n2 = 0,n3 = 1):
-    """This is the sum seires function that will return the nth value of a series of numbers. 1st option returns the nth number and the 2nd and 3rd option is the beginning of the series. Default is 0 and 1 for fibonacci"""
+    """This is the sum seires function that will return the nth value in a series of numbers. 1st option returns the nth number and the 2nd and 3rd option sets the beginning of the series. Default is 0 and 1 for fibonacci"""
     a=n2
     b=n3
     #comment out all but the last of the print statements if you don't want to see the count increase
@@ -47,7 +47,6 @@ def sum_series(n,n2 = 0,n3 = 1):
 
 #validating functions
 if __name__=="__main__":
-    #fibonacci(14)
     assert fibonacci(12) == 89
     assert lucas(12) == 199
     assert sum_series(8,2,1) == 29
