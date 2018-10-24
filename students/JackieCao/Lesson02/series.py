aaf33f0f025c9a99726eedb74374bd75dea31605
Shujin Cao (Jackie) <sc3977@columbia.edu>
2018-10-23 17:28:50 -0700

    re-submit a recursive solution for Fibonacci (#81)
    * switched to python version3 and fixed the indent
    
    * submit Fibonacci Series Exercise series.py
    
    * resubmit a recursive solution for Fibonacci

--- students/JackieCao/Lesson02/series.py
+++ students/JackieCao/Lesson02/series.py
@@ -1,82 +1,40 @@
 # Fibonacci Series
+# use recursion 
 
 def fibonacci(n):
-    """
-    make sure the input n is an integer and larger or equal to 0
-    """
-    if n != int(n):
-        if not isinstance(n,int):
-            print("n must be integer")
-    elif n < 0:
-        print("n must be larger or equal to 0")
+
+    if n == 0:
+        return 0	    
+    elif n == 1:
+        return 1
     else:
-        n = int(n)
-        if n >= 2:
-            """
-            make an empty list has length n+1, n start from 0 
-            the first two values are 0 and 1 for the Fibonacci series
-            """
-            L = [None]*(n+1)
-            L[0] = 0 
-            L[1] = 1
-            for i in range(2,n+1):
-                """
-                this equation is based on the Fibonacci definition
-                """
-                L[i] = L[i-2]+L[i-1] 
-            return(L[n]) #print the output
-        if n == 0:
-            return(0)	    
-        if n == 1:
-            return(1)	    
+        return fibonacci(n-2)+fibonacci(n-1)
+
 
 # Lucas Numbers
 
 
 def lucas(n):
-    # make sure the input n is an integer and larger or equal to 0
-    if n != int(n):
-        if not isinstance(n,int):
-            print("n must be integer")
-    elif n < 0:
-        print("n must be larger or equal to 0")
+
+    if n == 0:
+        return 2	    
+    elif n == 1:
+        return 1	    
     else:
-        n = int(n)
-        if n >= 2:
-            L = [None]*(n+1) # make an empty list has length n+1, n start from 0 
-            L[0] = 2 # the first two values are 2 and 1 for the Lucas series
-            L[1] = 1
-            for i in range(2,n+1):
-                L[i] = L[i-2]+L[i-1] # this equation is based on the Lucas definition
-            return(L[n]) # print the output
-        if n == 0:
-            return(2)	    
-        if n == 1:
-            return(1)	    
+        return lucas(n-2)+lucas(n-1)
+
 
 
 # sum_series
 
 def sum_series(n, n0=0, n1=1):
-    # make sure the input n is an integer and larger or equal to 0
-    if n != int(n):
-        if not isinstance(n,int):
-            print("n must be integer.")
-    elif n < 0:
-        print("n must be larger or equal to 0.")
+
+    if n == 0:
+        return n0	    
+    elif n == 1:
+        return n1
     else:
-        n = int(n)
-        if n >= 2:
-            L = [None]*(n+1) # make an empty list has length n+1, n start from 0 
-            L[0] = n0 # the first two values are n0 and n1
-            L[1] = n1
-            for i in range(2,n+1):
-                L[i] = L[i-2]+L[i-1] # this equation is based on the Fibonacci definition
-            return(L[n]) # print the output
-        if n == 0:
-            return(n0)	    
-        if n == 1:
-            return(n1)	    
+        return sum_series(n-2, n0=n0, n1=n1)+sum_series(n-1, n0=n0, n1=n1)
 
 
 if __name__ == "__main__":
@@ -97,7 +55,9 @@ if __name__ == "__main__":
     assert sum_series(5) == fibonacci(5)
     
     # test if sum_series matched lucas
-    assert sum_series(5,2,1) == lucas(5)
+
+    assert sum_series(5,n0=2,n1=1) == lucas(5)
+
 
     print("tests passed")
 
