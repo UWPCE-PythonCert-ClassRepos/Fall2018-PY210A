88c83a0c3c418092b30e979e2fc2b8a321e38e41
dancornutt <42428640+dancornutt@users.noreply.github.com>
2018-10-24 21:01:55 -0700

    Session02 HW (#84)
    * "added Readme"
    
    * Added typo
    
    * Changed Dir filepath, was in example_students...oops
    
    * removed accidental file!
    
    Signed-off-by: Dan Cornutt <dacornutt@gmail.com>
    
    * Submitting break_me.py for git practice
    
    * Adding the git ignore file
    
    * changed name of git ignore file
    
    * added local only folder
    
    * Notes and examples from class.
    
    * added more notes
    
    * Delete 2018-11-16_Notes.txt
    
    * changed file structure and added session02 modules.
    
    * added assertions for testing.
    
    * updated print_grid with correct spacing
    
    * updated ignore file to ignore local hw folder

---
+++ students/DanCornutt/session02/print_grid.py
@@ -0,0 +1,36 @@
+def dline(n):
+    print('+' + (n * ' -') + ' +' + (n * ' -') + ' +')
+
+def empline(n):
+    print('|' + (n * '  ') + ' |' + (n * '  ') + ' |')
+
+def print_grid(size):
+    if size < 3:
+        size = 3
+    # elif size == 11:
+    #     size = 9
+    mid = int((size - 1) / 2)
+    dline(mid)
+    for i in range(mid):
+        empline(mid)
+    dline(mid)
+    for i in range(mid):
+        empline(mid)
+    dline(mid)
+
+def str_build(outer, inner, anchor, middle):
+    str = anchor
+    for i in range(outer):
+        str = str + (inner * middle) + anchor
+    return str
+
+def print_grid2(outer,inner):
+    if outer < 1 or inner < 1:
+        print('I cannot make a grid with those dimensions.')
+    else:
+        for j in range(outer * inner + outer + 1):
+            if j == 0 or j % (inner + 1) == 0:
+                anchor, middle = '+', ' - '
+            else:
+                anchor, middle = '|', '   '
+            print(str_build(outer, inner, anchor, middle))
