8a4cc30ac2dea94bebc43c1b3c3a34e11ca6b74d
Kyle Barry <kylesbarry@gmail.com>
2018-10-25 18:45:35 -0700

    All series complete for list_lab exercise. You can uncomment certain series to run them. May come back to make minor tweaks/improvements

--- students/KyleBarry/session03/list_lab.py
+++ students/KyleBarry/session03/list_lab.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 def main():
+	#Series 1
 	fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
 
 	print("Here are all our current fruits:")
@@ -32,21 +33,50 @@ def main():
 		if i[0] == 'P':
 			print(i)
 
-		# Series 2
-	print(fruits)
-	#pop() removes the last item of the list by default
-	fruits.pop()
-	print(fruits)
-	while True:
-		to_delete = input('Which fruit would you like to delete? ')
-		try:
-			fruits.remove(to_delete)
-			break
-		except ValueError:
-			print("That's not in the list!")
+	# Series 2
+	# print(fruits)
+	# #pop() removes the last item of the list by default
+	# fruits.pop()
+	# print(fruits)
+
+	# while True:
+	# 	to_delete = input('Which fruit would you like to delete? ')
+	# 	try:
+	# 		fruits.remove(to_delete)
+	# 		break
+	# 	except ValueError:
+	# 		print("That's not in the list!")
+	# 		continue
 			
-	print(fruits)
+	# print(fruits)
 
 	#Series 3
+	# f = []
+	# for i in fruits:
+	# 	resp = input("Do you like {} ".format(i)).lower()
+	# 	while True:
+	# 		if resp[0] == 'n':
+	# 			#if .remove() here, for loop skips new index of next value
+	# 			f.append(i)
+	# 			break
+	# 		if resp[0] == 'y':
+	# 			break
+	# 		else:
+	# 			resp = input('Please say yes or no: ').lower()
+
+	# for i in f:
+	# 	fruits.remove(i)
+
+	# print(fruits)
+
+	#Series 4
+	frutki = fruits[:]
+	for i in range(len(frutki)):
+		frutki[i] = frutki[i][::-1]
+
+	fruits.pop()
+	print(fruits)
+	print(frutki)
+
 
 main()
