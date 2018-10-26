8aa4ad62f1b266c718a5c86e626656932929d769
D Smith <dsmith82@uw.edu>
2018-10-25 23:50:51 -0700

    List_lab series 2 complete with bonus

--- students/DrewSmith/session03/list_lab.py
+++ students/DrewSmith/session03/list_lab.py
@@ -1,39 +1,45 @@
 #!/usr/bin python3
 
-def series_01():
-    fruits = ["Apples", "Pears", "Oranges", "Peaches"]
-    print(fruits)
-    new_fruits = input("Enter a new fruits: ")
-    fruits.append(new_fruits)
-    print(fruits)
-    index = int(input(f"Give a number between 1 and {len(fruits)}: "))
-    print("You chose: " + (fruits[index - 1] if index > 0 and index <= len(fruits) else "Error!"))
+#Series 1
+fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+print(fruits)
+new_fruits = input("Enter a new fruits: ")
+fruits.append(new_fruits)
+print(fruits)
+index = int(input(f"Give a number between 1 and {len(fruits)}: "))
+print("You chose: " + (fruits[index - 1] if index > 0 and index <= len(fruits) else "Error!"))
 
-    print()
-    fruits = ["Grapes"] + fruits
-    print(fruits)
+print()
+fruits = ["Grapes"] + fruits
+print(fruits)
 
-    fruits.insert(0, "Plums")
-    print(fruits)
+fruits.insert(0, "Plums")
+print(fruits)
 
-    print()
-    print("Fruits that tart with P:")
-    for fruit in fruits:
-        if fruit[:1].lower() == "p":
-            print(fruit)
+print()
+print("Fruits that tart with P:")
+for fruit in fruits:
+    if fruit[:1].lower() == "p":
+        print(fruit)
 
-def series_02():
-    print()
-    print("Series 2:")
-    print(fruits)
-    del fruits[-1:]
-    print(fruits)
+
+#Series 2
+print()
+print("Series 2:")
+print(fruits)
+del fruits[-1:]
+
+print(fruits)
+fruits = fruits * 2
+print("Doubling Fruit List")
+print(fruit)
+found = False
+while found is False:
     find = input("What fruit should we remove?: ")
+
     for fruit in fruits:
         if fruit == find:
             fruits.remove(fruit)
-    print(fruits)
+            found = True
 
-if __name__ == '__main__':
-    series_01()
-    series_02()
+print(fruits)
