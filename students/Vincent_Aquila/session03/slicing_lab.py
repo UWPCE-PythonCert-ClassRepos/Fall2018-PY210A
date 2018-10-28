fdbdb48eda5868694e35665d643c71bd56c2d5ea
Vincent Aqulia <aquila.vincent.m@gmail.com>
2018-10-28 15:41:25 -0700

    modifying slicing lab task 1 using slicing methods (had been using indexing methods); added output text in terminal so user can see identification of each problem; still unable to make assertion work

--- students/Vincent_Aquila/session03/slicing_lab.py
+++ students/Vincent_Aquila/session03/slicing_lab.py
@@ -18,61 +18,48 @@ def exchange_first_last(seq):
     return a_new_sequence
 """
 
-
-
-#1) with the first and last items exchanged
+#item 1
+print("---Item 1: with the first and last items exchanged")
 def swap_first_last(list):
-    x = len(list)
-    print(x-1) #test-output test shows x = 8, or 8 indicies
-    temp = list[8] #the number 9 is placed in the variable called 'temp'
-    print(temp) #test-output is the number 9
-    list[8] = list[0] #the number 1 is now where the number 9 is located
-    print(list[8]) #test-shows that the number 1 is in index 8
-    list[0] = temp #the number 9 has been shifted to the first index
-    new_list = list
-    print(new_list) #new list with numbers 1 and 9 swapped
+    x = list[:1]
+    list[:1] = list[-1:]
+    list[-1:] = x
+    print(list)
+swap_first_last(list = ([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
-swap_first_last(list = [1, 2, 3, 4, 5, 6, 7, 8, 9])
+#item 2
+print("---Item 2: with every other item removed")
+def remove_every_other(list2):
+    print(list2[0:9:2])
+remove_every_other(list2 = ([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
+#item 3
+print("---Item 3: with the first 4 and the last 4 items removed, and then every other item in the remaining sequence")
+def first_4_last_4(list3):
+    y = list3[4:] #removes first four
+    y1 = y[0:9:2]
+    print(y1)
+    z = list3[:4] #removes last four
+    z1 = z[0:9:2]
+    print(z1)
+first_4_last_4(list3 = ([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
+#item 4
+print("---Item 4: with the elements reversed (just with slicing)")
+def reverse_list(list4):
+    print(list4[::-1])
+reverse_list(list4 = ([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
-#2) with every other item removed
-def remove_every_other(list):
-    print(list[0:9:2])
-
-remove_every_other(list = [1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-
-
-#3) with the first 4 and the last 4 items removed, and then every other item in the remaining sequence
-def first_4_last_4(list):
-    print(list[4:]) #removes first four
-    print(list[:4]) #removes last four
-
-first_4_last_4(list = [1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-
-
-#4) with the elements reversed (just with slicing)
-def reverse_list(list):
-    print(list[::-1])
-
-reverse_list(list = [1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-
-
-#5) with the last third, then first third, then the middle third in the new order
-def thirds(list):
-    i = list[:3]
-    j = list[3:6]
-    k = list[6:9]
+#item 5
+print("---Item 5: with the last third, then first third, then the middle third in the new order")
+def thirds(list5):
+    i = list5[:3]
+    j = list5[3:6]
+    k = list5[6:9]
     print(k,i,j)
+thirds(list5 = ([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
-thirds(list = [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-
-"""
 #run tests - this isn't working - will get help later
-if __name__ == "__main__":
-    assert swap_first_last(new_list)  == [9, 2, 3, 4, 5, 6, 7, 8, 1]
-"""
+#if __name__ == "__main__":
+#    assert swap_first_last([1, 2, 3, 4, 5, 6, 7, 8, 9]) == [9, 2, 3, 4, 5, 6, 7, 8, 1]
