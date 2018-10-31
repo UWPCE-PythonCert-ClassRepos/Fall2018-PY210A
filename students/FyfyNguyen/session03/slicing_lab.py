33a37ddb2a826893424716a129eb36dbe1ce4b46
Fyfy Nguyen <fyfy@downstreamimpact.com>
2018-10-30 19:35:51 -0700

    Update slicing lab

--- students/FyfyNguyen/session03/slicing_lab.py
+++ students/FyfyNguyen/session03/slicing_lab.py
@@ -1,21 +1,34 @@
+#!/usr/bin/env python3
+
 """
 Write some functions that take a sequence as an argument, and return a copy of that sequence:
 """
 # with the first and last items exchanged
 def exchange_first_last(seq):
     n = len(seq)
-    if n <= 1:
-        return seq
+    if n < 2:
+        return seq[:]
     first = seq[0:1]
     last = seq[n-1:n]
     mid = seq[1:-1]
     return last + mid + first
-print(exchange_first_last((1,)))
 
 # with every other item removed
+def remove_every_other(seq):
+    return seq[::2]
+
 # with the first 4 and the last 4 items removed, and then every other item in the remaining sequence
+def four_removed(seq):
+    return seq[4:-4:2]
+
 # with the elements reversed (just with slicing)
+def reversed(seq):
+    return seq[::-1]
+
 # with the third, then first third, then the middle third in the new order
-
-
-
+def switch_thirds(seq):
+    if len(seq) < 3:
+        return seq[:]
+    third = len(seq) // 3
+    return seq[-third:]+ seq[:third] + seq[third:-third]
+    
