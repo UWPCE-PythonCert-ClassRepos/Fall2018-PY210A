481c1698c4834b00fef238bab0996e314c603be7
Kyle Barry <kylesbarry@gmail.com>
2018-10-31 22:31:59 -0700

    Added switch dictionary functionality, but need to create a sub dispatch for thank you function. Also need to adjust file writing output and give something to the user

--- students/KyleBarry/session03/mailroom.py
+++ students/KyleBarry/session03/mailroom.py
@@ -1,4 +1,6 @@
+#!/usr/local/bin/python3
 #Establish some data to work with in a dictionary
+
 from datetime import datetime
 
 donors = {"Timothy Tander": [100, 200, 300],
@@ -7,49 +9,50 @@ donors = {"Timothy Tander": [100, 200, 300],
            "Sandra Connors": [1800, 2300, 7000],
            "Betsy Hammond": [500, 190, 212, 55]}
 
-donors["Timothy Tander"].append(800)
+prompt = ("Please enter one of the following:\n 1: Send a thank you note\n 2: Create a report\n 3: Quit\n")
 
-def thank_you(full_name, donation):
+def thank_you():
+ 
     now = datetime.now().strftime("%Y-%m-%d")
-    with open(f"{full_name}_{now}", 'w') as f:
-        f.write(f"Dear {full_name}\n\n")
-        f.write(f"\tThank you so much for your generous donation of {donation}!\n\n")
-    
+    full_name = input("Please enter a name (list for full list of donors): ")
+    while True:
+        if full_name == 'list':
+            for i in donors:
+                print(i)
+            break
+        elif full_name in donors:
+            donation = float(input('How much would you like to donate? '))
+            donors[full_name].append(donation)
+        else:
+            donation = float(input('How much would you like to donate? '))
+            donors[full_name] = [donation]
+
+        with open(f"{full_name}_{now}", 'w') as f:
+            f.write(f"Dear {full_name}\n\n")
+            f.write(f"\tThank you so much for your generous donation of {donation}!\n\n")
+            break
+
 def make_report():
 
     print("{:^20}|{:^20}|{:^20}|{:^20}".format("Donor Name", "No. Donations","Avg. Donation", "Total Donations"))
     for k,v in donors.items():
         print("{:<20}|{:>20}|{:>20}|{:>20}".format(k, len(v), sum(v)/len(v),
                                                    sum(v)))
- 
-def main():
+
+def exiting():
+    print("Quitting program!")
+    quit()
+
+def main(prompt, options_dict):
     print('Welcome to the mailroom')
-    answer = ' '
-    while answer[0].lower() != 'q':
-        print("Please select one of the following options:")
-        print("Quit: 'q' \n"
-              "Thank you: 't' \n"
-              "Report: 'r' \n")
-        answer = input(' ')
-        answer = answer[:1].lower().strip()
-        if answer == 't':
-            full_name = input('Please enter a full name (list for donors) ')
-            if full_name == 'list':
-                for i in donors.keys():
-                    print(i)
-            elif full_name in donors.keys():
-                donation = float(input('How much would you like to donate? '))
-                donors[full_name].append(donation)
-                thank_you(full_name, donation)
-            elif full_name not in donors.keys():
-                donation = float(input('How much would you like to donate? '))
-                donors[full_name] = [donation]
-                thank_you(full_name, donation)
-        elif answer == 'r':
-            make_report()
+    while True:
+        answer = input(prompt)
+        options_dict[answer]()
+
+options_dict = {"1": thank_you, "2": make_report, "3": exiting}
 
 if __name__ == '__main__':
-    main()
+    main(prompt, options_dict)
 
 
 
