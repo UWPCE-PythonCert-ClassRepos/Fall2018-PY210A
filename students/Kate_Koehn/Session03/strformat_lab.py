b603d90d9a574bdeb7ff5d4d29c11fc21098a910
Kate Koehn <koehn.kathryn@gmail.com>
2018-11-03 11:44:03 -0700

    completed program

--- students/Kate_Koehn/Session03/strformat_lab.py
+++ students/Kate_Koehn/Session03/strformat_lab.py
@@ -4,11 +4,11 @@ print("file_{:03d}: {:.2f}, {:.2E}, {:.2E}".format(*n))
 
 #task two - alternate type of formatting
 n = (2, 123.4567, 10000, 12345.67)
-print("file_%03d: %.2f, %.2E, %.2E" % (n))
+print("file_%03d: %.2f, %.2E, %.2g" % (n))
 
 #task three
 def formatter(tup):
-    return "the {} numbers are: {:*}".format(len(tup), *tup)
+    return "the {} numbers are: {}".format(len(tup), tup)
 
 n = (2, 123.4567, 10000, 12345.67)
 formatter(n)
