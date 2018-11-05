4b8a89b88c0575314292d6ebea3a304ddd1da02c
HABTAMU ASFAW <asfawh@uw.edu>
2018-11-04 20:10:16 -0800

    P1: mailroom and session03 strformat Lab

--- students/HABTAMU/session03/mailroom.py
+++ students/HABTAMU/session03/mailroom.py
@@ -1,85 +1,79 @@
-#!/usr/bin/env python3
-
+#!/usr/bin python3
 import sys
 
-donors_info = [("Bill Gates", 3500, 5500, 7500), 
-               ("Paul Alen", 3000, 3700, 3900), 
-               ("Jeff Benzo", 3300, 5000, 7500),
-               ("Mark Zuckerberg", 33565.37, 465.37, 545.37, 7506),
-               ("Warren Buffett", 3303.17, 334.17, 5080, 7500)
-              ]
-t = tuple(donors_info[:]) 
-name = []       
-prompt = "\n".join(("\n Please choose what you intended to know!:",
-           " Choices are ?:",
-           " 1 - Send a Thank you!",
-           " 2 - Create a Report" ,
-           " 3 - Exit",
-           " >>> "))
-def list():
-    for i in range(len(t)):
-        print(t[i][0])
+donors_info = [("Bill Gates", [3500, 5500, 7500]),
+               ("Paul Alen", [3000, 3700, 3900]),
+               ("Jeff Benzo", [3300, 5000, 7500]),
+               ("Mark Zuckerberg", [33565.37, 465.37, 545.37, 7506]),
+               ("Warren Buffett", [3303.17, 334.17, 5080, 7500])
+               ]
+
+prompt = "\n".join(("\n Please choose from the following!:",
+                    " Choices are ?:",
+                    " T - To Send a Thank you!",
+                    " R - To Create a Report",
+                    " Q - To Exit",
+                    " >>> "))
+
+def send_thankyou():
+    d_name = "list"
+    while d_name == "list":
+        d_name = input("Enter a donor name Or Type \'list' if you don't know the name ?: ")
+        if d_name == "list":
+            for name in donors_info:
+                print(f"- {name[0]}")
+            continue
+
+    for name in donors_info:
+        if name[0] == d_name:
+            new_name = name
+            break
+    else:
+        print("New Donor added!")
+        new_name = (d_name, [])
+        donors_info.append(new_name)
+
+    contribution = float(input(f"Enter {d_name} contribution :? "))
+    new_name[1].append(contribution)
+    print()
+    print("""
+    Dear {:<10}\n,
+    Thank you for your generous donation ${:,.2f}.
+    """.format(d_name, contribution))
+    print("\n Best regards,\n Your Youth and Seniors Foundation \n")
+
 
 def create_report():
-    """
-    Let's choose an options from the list
-    Args:
-        num: options(1 - 4) to choose
-    Returns:
-        A list of str contain from the doc
-    """
-    print("What if?!")
+    print()
+    print("{:<20}| {:<10} | {:<10} | {:<10}".format("Donor Name", "Total Given", " Num Gifts", "Average Gift"))
+    print("-" * 60)
+    # donors_info.sort(key=report_sort, reverse=True)
+    for name in donors_info:
+        print("{:<21} ${:>11.2f} {:>12} ${:>12.2f}".format(
+            name[0], sum(name[1]), len(name[1]), sum(name[1]) / len(name[1])))
 
-def send_thankyou(name):
-
-    name = input("What is donors name? Type list if you don't know the name \n:> ").strip()
-    # Carlos Slim
-    if name == 'list':
-        list()
-
-    elif name in donors_info:
-        print("Hello Mr {}!".format(name))
-        donation_amount = input("Hi, Mr {} how much would you like to donat".format(name).strip().lower())
-        donors_info.append(tuple(name,donation_amount))
-        print(donors_info)
-        
-    elif name not in donors_info:
-        add_donor = input("Donor's name {} is not in the donors list, Would you like to add to the donors list (y/n)?:".format(name).strip().capitalize())
-        if  add_donor == "y":
-            donors_info.append(tuple([name]))
-            print(donors_info)
-        elif add_donor == "n":
-            print("No problem, Next time!")
-
-    # else:
-    #     return 0
-
-    # elif name Carlos Slimin donors_info[0]:
-    #     print("No donor name exist with that name:")
-    # else:
-    #     return 0
-        # new_donor = input.print("No in donor list, do you want to add?: y/N")
 
 def exit_program():
     print("Bye!")
-    sys.exit() # exit the interactive script
-
+    sys.exit()  # exit the interactive script
 
 def main():
     while True:
         response = input(prompt)
 
-        if response == "1":
-            send_thankyou(name)
+        if response == "T":
+            send_thankyou()
 
-        elif response == "2":
+        elif response == "R":
             create_report()
 
-        elif response == "3":
+        elif response == "Q":
             exit_program()
-            
+
         else:
             print("No a valid option")
 
+
 if __name__ == '__main__':
     main()
+
