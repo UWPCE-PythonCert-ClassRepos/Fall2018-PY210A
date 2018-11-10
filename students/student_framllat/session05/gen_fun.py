9b6caa336f5bc1ba29d3e4cb012262c288469576
Ronald F Tallman <framllat@gmail.com>
2018-11-10 06:14:10 -0800

    More Lab1 gen_fun funct -Pytest

---
+++ students/student_framllat/session05/gen_fun.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def gen_fun(*args, **kwargs):
+    # print("The positionals aka *args:", args)
+    # print("The keywords aka **kwargs:", kwargs)	
+	return(args, kwargs)
+
+	            
+    
+    
