d680a42580d4a83ce3518aeedd11d07086736677
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-11-12 15:45:31 -0800

    Updating completed list_lab.py...ready to grade!

--- students/C_Farris/session03/list_lab.py
+++ students/C_Farris/session03/list_lab.py
@@ -32,7 +32,7 @@ def addToBeginningOfList(myFruit):
 
 def askforAFruit():
     newFruit = input("Please enter a different fruit ==>")
-    newFruit.strip()  # strip any whitespace
+    newFruit = newFruit.strip().capitalize()  # strip any whitespace
     myFruit.append(newFruit)
     print(myFruit)
 
@@ -47,10 +47,62 @@ def askForANumber():
     print(fruitNumb, gotThatFruit)
 
 
+def rm_A_fruit():
+    print("rm_A_fruit")
+    print(myFruit)
+    lastItem = len(myFruit) - 1
+    myFruit.pop(lastItem)
+    print(myFruit)
+
+
+def get_fruit_to_rm():
+    findFruit = input('Please select a fruit to remove from list above ==>')
+    findFruit = findFruit.strip().capitalize()
+    myFruit.remove(findFruit)
+    print(myFruit)
+
+
+def rm_disliked_fruit():
+    print(myFruit)
+    fruitToDelete = []
+    for x in myFruit:
+        question = ""
+        verdict = ''
+        while verdict != 'yes' and verdict != 'no':
+            question = "Do you like {}? Yes or No? ==>  ".format(x.lower())
+            verdict = input(question)
+            verdict = verdict.strip().lower()
+            if verdict == 'no':
+                fruitToDelete.append(x)
+    findAndDeleteFruit(fruitToDelete)
+    print(myFruit)
+
+
+def findAndDeleteFruit(fruitToDelete):
+    for x in fruitToDelete:
+        myFruit.remove(x)
+
+
+def make_new_reversed_list():
+    newFruity = []
+    for x in myFruit:
+        newFruity.append(x[::-1])
+    print("The new list with words reversed: {}".format(newFruity))
+    print("below is the beginning list: {}".format(myFruit))
+
+
 if __name__ == '__main__':
     myFruit = ['Apples', 'Pears', 'Oranges', 'Peaches']
+    """series 1"""
     print(myFruit)
     askforAFruit()
     askForANumber()
     addToBeginningOfList(myFruit)
     displayPfruits(addToBeginningOfList(myFruit))
+    """series 2"""
+    rm_A_fruit()
+    get_fruit_to_rm()
+    """series 3"""
+    rm_disliked_fruit()
+    """series 4"""
+    make_new_reversed_list()
