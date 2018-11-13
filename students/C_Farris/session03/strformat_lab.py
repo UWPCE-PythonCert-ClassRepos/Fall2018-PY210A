c2b23c9734f26e839a4c454648f88c7c596907e5
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-11-13 11:43:46 -0800

    commiting completed string formatting lab.
    I struggled with making long f. formatting strings PEP8 compliant.
    I have a few lines of long f.strings that the Anaconda linter dislikes.
    Any feedback for correct repositioning of the string would be appreciated!

--- students/C_Farris/session03/strformat_lab.py
+++ students/C_Farris/session03/strformat_lab.py
@@ -49,6 +49,7 @@ def buildUpStringsTwo():
                   stringPieces[3])
     print(finalString)
 
+
 def buildUpStringsThree():
     stringPieces = []
     count = 0
@@ -70,15 +71,33 @@ def buildUpStringsThree():
         count += 1
     print(form_string.format(*stringPieces))
 
+
 def buildUpStringsFour():
     mytuple = (4, 30, 2017, 2, 27)
-    myString = "{:0>2d} {} {} {:0>2d} {}".format(mytuple[3],mytuple[4],mytuple[2],mytuple[0], mytuple[1])
+    myString = "{:0>2d} {} {} {:0>2d} {}".format(mytuple[3],
+               mytuple[4], mytuple[2], mytuple[0], mytuple[1])
     print(myString)
+
 
 def beefUpFruitString():
     fruityList = ['oranges', 1.3, 'lemons', 1.1]
     print(f"The weight of an {fruityList[0][:-1]} is {fruityList[1]} and the weight of a {fruityList[2][:-1]} is {fruityList[3]}")
     ##here you need to add the 20% and reprint
+    print(f"The weight of an {fruityList[0][:-1]} is {fruityList[1]*1.2} and the weight of a {fruityList[2][:-1]} is {fruityList[3]*1.2}")
+
+
+def practicePrintingColumns():
+    practiceList = [['Gary', 5, 2], ['Tom', 15, 30],
+                    ['Richard', 80, 100], ['Jerry', 25, 1000],
+                    ['Mary', 30, 20000]]
+    print('\n')
+    print('{:<20}'.format('Name'), '{:<15}'.format('Age'),
+          '{:>5}'.format('Cost'))
+    print('\n')
+    for x in practiceList:
+        print('{:<20}'.format(x[0]), '{:<15}'.format(x[1]),
+             '{:>5,}'.format(x[2]))
+
 
 if __name__ == '__main__':
     practiceTuple = (2, 123.4567, 10000, 12345.67)
@@ -92,3 +111,5 @@ if __name__ == '__main__':
     buildUpStringsFour()
     """Task 5"""
     beefUpFruitString()
+    """Task 6"""
+    practicePrintingColumns()
