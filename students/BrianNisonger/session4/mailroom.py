a158a3eadce249d4ed7c6cf508d1abd6b4e4a2d4
Brian Nisonger <vanulor@gmail.com>
2018-11-13 00:49:21 -0800

    Updated mailroom to use comprehensions, and exceptions, reworked report function, and send report functions

--- students/BrianNisonger/session4/mailroom.py
+++ students/BrianNisonger/session4/mailroom.py
@@ -1,7 +1,13 @@
 import statistics 
 
+donors={
+"Bill Gates":[100.00,200.00,300.00],
+"Jack Frost":[2.00,5.00,200.00],
+"Maya Angelou":[20000.00,20.00,1234.56]
+}   
+
 def sort_key(item):
-    return item[1]
+    return item[1][1]
     
 def thank_you():
     full_name="list"
@@ -13,8 +19,13 @@ def thank_you():
         else:
             if full_name not in donors:
                 donors[full_name]=[]
-            donation=input("Please enter a donation==>")
-            donation=float(donation)
+            while True: 
+                try:
+                    donation=float(input("Please enter a donation==>"))
+                except ValueError:
+                    print("Please enter a numerical donation")
+                else:
+                    break
             donors[full_name].append(donation)
             thank_you_string=f'Dear {full_name}, Thank you for your donation of {donation:.2f}.  These funds help save the migratory butterflies of New South East.  Thank you'
             file_name=full_name.replace(" ","_")
@@ -23,69 +34,58 @@ def thank_you():
                 outfile.write(thank_you_string)
 
 def report():
-    sum_donor={}
-    num_donations={}
-    average_donor={}
-    temp=[]
-    sum_donors=[]
     headers=("Donor Names","Total Given","Num Gifts","Average Gifts")
-    header_string=f'{headers[0]:<15} {headers[1]:<15} {headers[2]:<15} {headers[3]:<15}'
+    header_string=f'{headers[0]:<20} {headers[1]:<15} {headers[2]:<15} {headers[3]:<15}'
     print(header_string)
-    for donor in donors:
-        sum_donor[donor]=sum(donors[donor])
-        num_donations[donor]=len(donors[donor])
-        average_donor[donor]=statistics.mean(donors[donor])
-    for donor_name in sum_donor:
-        sum_donors_tuple=[donor_name,sum_donor[donor_name]]
-        sum_donors.append(sum_donors_tuple)
+    sum_donors=[[k,[average_donations(v),total_donations(v),count_donations(v)]] for (k,v) in donors.items()]
     sum_donors=sorted(sum_donors, key=sort_key,reverse=True)
-    for sorted_donors in sum_donors:
-        report_string=f'{sorted_donors[0]:<15} ${sorted_donors[1]:<15.2f} {num_donations[sorted_donors[0]]:<15} ${average_donor[sorted_donors[0]]:<15.2f}'
-        print(report_string)
-      
-        
+    report_strings=[f'{donor[0]:<20} ${donor[1][1]:<15.2f} {donor[1][2]:<15} ${donor[1][0]:<15.2f}' for donor in sum_donors]
+    for string in report_strings:
+        print(string)
+    
+def average_donations(amount):
+    return statistics.mean(amount)
+
+def total_donations(amount):
+    return sum(amount)
+
+def count_donations(amount):
+    return len(amount)
 
 def send_donors():
-     sum_donors={}
-     for donor in donors:
-        sum_donors[donor]=sum(donors[donor])
-     for sum_donor in sum_donors:   
-        thank_you_string=f'Dear {sum_donor}, Thank you for your donation of {sum_donors[sum_donor]:.2f}.  These funds help save the migratory butterflies of New South East.  Thank you'
-        file_name=sum_donor.replace(" ","_")
-        file_name=f'{file_name}.txt'
-        with open(file_name,"w") as outfile:
-            outfile.write(thank_you_string)
-
-def quit():
-    pass
+    total_donors=[[k,total_donations(v),f'{k.replace(" ","_")}.txt'] for (k,v) in donors.items()]
+    thank_you_string=[[f'Dear {donor[0]}, Thank you for your donation of {donor[1]:.2f}.  These funds help save the migratory butterflies of New South East.  Thank you',donor[2]] for donor in total_donors]
+    for t_string in thank_you_string:
+        with open(t_string[1],"w") as outfile:
+            outfile.write(t_string[0])
 
 
-    
-    
-donors={
-"Bill Gates":[100.00,200.00,300.00],
-"Jack Frost":[2.00,5.00,200.00],
-"Maya Angelou":[20000.00,20.00,1234.56]
-}    
+def unknown():
+    print("Please enter a valid response")
+    return None 
 
 def main():
     menu={
     "t":thank_you,
     "r":report,
     "s":send_donors,
-    "q":quit
     }
     answer=""
-    while answer !="q":
+    while True:
         print("Please enter one of the following")
-        answer=input("t to Thank a Single Donor\n"
-                     "r to Send a Report\n"
-                     "s to Send all Donors a Thank You Email\n"
-                     "q to Quit:")
-        answer=answer[0].lower()
-        choices=("t","r","s","q")
-        if answer in choices:
-            menu.get(answer)()
+        try:
+            answer=input("t to Thank a Single Donor\n"
+                         "r to Send a Report\n"
+                         "s to Send all Donors a Thank You Email\n"
+                          "q to Quit:")
+            answer=answer[0].lower()
+            if answer=="q":
+                break
+            menu.get(answer,unknown)()
+        except KeyboardInterrupt:
+            break
+            
+           
 
 if __name__ == "__main__":
     main()
