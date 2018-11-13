1b0e5bb413624a9b6363895a9f2e2c75dd49e1bd
HABTAMU ASFAW <asfawh@uw.edu>
2018-11-12 18:44:29 -0800

    session04 list of exercises

--- students/HABTAMU/session04/trigrams_ex4.py
+++ students/HABTAMU/session04/trigrams_ex4.py
@@ -5,52 +5,9 @@ import sys
 
 f_name = "sherlock_small.txt"
 
-# def make_words(txt):
-#     txt_table = {}
-
-
-def build_txt(trigrams):
-    """
-    Build up some new “fake” text, first pick a random key pair to start with from a dict trigrams.
-    arguments:
-        dict trigrams
-    return:
-        fake_txt[] list
-    """
-    
-    fake_txt = []
-    pair_txt = random.choice(list(trigrams.keys()))
-    fake_txt.extend(pair_txt)
-
-    while len(fake_txt) < 50:
-        pair = tuple(fake_txt[-2:])
-        try:
-            followers = trigrams[pair]
-        except KeyError:
-            print(" this pair is not there", pair)    
-            pair = random.choice(list(trigrams.keys()))
-            followers = trigrams[pair]
-        
-        one_word = random.choice(followers)
-        fake_txt.append(one_word)
-        
-    print(fake_txt)
-    x = " ".join(fake_txt)
-    return x
-
-def strip_punc(my_punc):
-    """replace the following list of punctuation with an empity space """
-
-    punc = [',', '--','(',')','.']
-    for p in punc:
-        my_punc = my_punc.replace(p,' ')
-    return my_punc.strip()
-
-
 def read_in_data(f_name):
     """
     read the contents of a project Gutenberg book
-
     returns it as one big string
     """
     with open(f_name) as f:
@@ -61,11 +18,11 @@ def read_in_data(f_name):
             line = line.split()
             words.extend(line) 
     return words
-    
+
 
 def build_trigrams(words):
     """build a trigram dict from the passed-in list of words"""
-    
+
     trigrams = {}
     for i in range(len(words) - 2):
         pair = tuple(words[i:i+2])
@@ -74,37 +31,57 @@ def build_trigrams(words):
             trigrams[pair].append(follower)
         else:
             trigrams[pair] = [follower]
-        
+
     return trigrams
 
+
+def strip_punc(my_punc):
+    """We need to strip out those punctuation or replace the following list of punctuation with an empity space """
+
+    punc = [',', '--', '(', ')', '.']
+    for p in punc:
+        my_punc = my_punc.replace(p, ' ')
+    return my_punc.strip()
+
+
+def build_txt(trigrams):
+    """
+    Build up some new “fake” text, first pick a random key pair to start with from a dict trigrams.
+    arguments:
+        dict trigrams
+    return:
+        list: fake_txt[]
+    """
+
+    fake_txt = []
+    pair_txt = random.choice(list(trigrams.keys()))
+    fake_txt.extend(pair_txt)
+
+    while len(fake_txt) < 50:
+        pair = tuple(fake_txt[-2:])
+        try:
+            followers = trigrams[pair]
+        except KeyError:
+            print(" this pair is not there", pair)
+            pair = random.choice(list(trigrams.keys()))
+            followers = trigrams[pair]
+
+        one_word = random.choice(followers)
+        fake_txt.append(one_word)
+
+    print(fake_txt)
+    x = " ".join(fake_txt)
+    return x
+
+
 if __name__ == "__main__":
-    # get the filename from the command line
+    #get the filename from the command line
+    words = read_in_data(f_name)
     # try:
     #     f_name = sys.argv[1]
     # except IndexError:
     #     print("Youn must pass in a filename")
     #     sys.exit(1)
-        
-    words = read_in_data(f_name)
     trigrams = build_trigrams(words)
     new_txt = build_txt(trigrams)
     #print(new_txt)
-
-
-
-
-
-# if __name__ == "__main__":
-#     # get the filename from the command line
-#     try:
-#         filename = sys.argv[1]
-#     except IndexError:
-#         print("You must pass in a filename")
-#         sys.exit(1)
-
-#     in_data = read_in_data(filename)
-#     words = make_words(in_data)
-#     word_pairs = build_trigram(words)
-#     new_text = build_text(word_pairs)
-
-#     print(new_text)
