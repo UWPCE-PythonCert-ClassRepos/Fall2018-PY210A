e3572fdd4ce2e85695a3c0b048f03b1dba8944f5
HABTAMU ASFAW <asfawh@uw.edu>
2018-11-12 20:42:12 -0800

    previous exercise someupdate and revisions

--- students/HABTAMU/session02/fibonacci_serios.py
+++ students/HABTAMU/session02/fibonacci_serios.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 def fibonacci(n):
     a, b = 0, 1
-
     if n <= 0:
         return 1
     else:
         for i in range(n):
             print(a, end=" ")
-
             c = a + b
             a = b
             b = c
