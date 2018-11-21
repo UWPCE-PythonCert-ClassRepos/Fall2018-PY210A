a96795adcfd823f94db856ebf319b75a61042eef
Ronald F Tallman <framllat@gmail.com>
2018-11-21 09:06:13 -0800

    Marching thru the rendererrr....

--- students/student_framllat/session07/html_render.py
+++ students/student_framllat/session07/html_render.py
@@ -8,19 +8,36 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element:
 
+    tag = "html"
+    
     def __init__(self, content=None):
-        self.content = [content]
+    	if content is None:
+    		self.content = []
+    	else:
+            self.content = [content]
 
     def append(self, new_content):
         self.content.append(new_content)
 
     def render(self, out_file):
-            out_file.write("<html>"line)
+        out_file.write("<{}>\n".format(self.tag))
         for line in self.content:
             out_file.write(line)
             out_file.write("\n")
+        out_file.write("</{}>\n".format(self.tag))
 
-#<html>
-#Some content.
-#Some more content.
-#</html>
+
+class Html(Element):
+    tag = "html"
+
+
+class Body(Element):
+    tag = "body"
+
+class P(Element):
+    tag = "p"
+
+
+
+
+
