42f26f6c3b26f6e9d09f2bfdda1c62a771ec2c2b
Tim Pauley <timothyjpauley@gmail.com>
2018-11-20 20:41:06 -0800

    commiting data

--- students/TimPauley/session06/cigar_party.py
+++ students/TimPauley/session06/cigar_party.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+#Tim Pauley
+#Python 201a
+# Nov 20 2018
+
 """
 When squirrels get together for a party, they like to have cigars.
 A squirrel party is successful when the number of cigars is between
@@ -11,5 +15,48 @@ or False otherwise.
 """
 
 
-def cigar_party(cigars, is_weekend):
-    pass
+def test_1():
+    assert cigar_party(30, False) is False
+
+
+def test_2():
+
+    assert cigar_party(50, False) is True
+
+
+def test_3():
+
+    assert cigar_party(70, True) is True
+
+
+def test_4():
+    assert cigar_party(30, True) is False
+
+
+def test_5():
+    assert cigar_party(50, True) is True
+
+
+def test_6():
+    assert cigar_party(60, False) is True
+
+
+def test_7():
+    assert cigar_party(61, False) is False
+
+
+def test_8():
+    assert cigar_party(40, False) is True
+
+
+def test_9():
+    assert cigar_party(39, False) is False
+
+
+def test_10():
+    assert cigar_party(40, True) is True
+
+
+def test_11():
+    assert cigar_party(39, True) is False
+
