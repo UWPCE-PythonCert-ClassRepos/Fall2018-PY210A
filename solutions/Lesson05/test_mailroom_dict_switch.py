efa0526dd2879b3d7bdfcf5019a2d0c156ec4478
Jae Kim <kjae>
2018-11-22 12:38:05 -0800

    Adding work

--- solutions/Lesson05/test_mailroom_dict_switch.py
+++ solutions/Lesson05/test_mailroom_dict_switch.py
@@ -1,16 +1,24 @@
 import mailroom_dict_switch as mr
 
+<<<<<<< Updated upstream
 
 def test_remove_inputquotes():
     result = mr.remove_inputquotes('"this"')
     assert result == 'this'
 
+=======
+def test_remove_inputquotes():
+    result = mr.remove_inputquotes('"this"')
+
+    assert result == 'this'
+>>>>>>> Stashed changes
 
 def test_remove_inputquotes_single():
     result = mr.remove_inputquotes("'this'")
 
     assert result == 'this'
 
+<<<<<<< Updated upstream
 
 def test_make_donor_list():
     result = mr.make_donor_list()
@@ -22,3 +30,16 @@ def test_make_donor_list():
     # assert False
 
 
+=======
+#pytest parameterized testing
+
+def test_make_donor_list():
+    result = mr.make_donor_list()
+    print(result)
+
+    num_donors = len(mr.DONORS)
+
+    assert len(result.split("\n")) == num_donors + 1
+    assert result.startswith("Donor Names:")
+    #assert False
+>>>>>>> Stashed changes
