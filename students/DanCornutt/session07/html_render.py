bce1cb326237fc2e032690f6059dd34513731a3f
Dan Cornutt <dacornutt@gmail.com>
2018-11-26 21:32:23 -0800

    Through step 4 of HTML parser

--- students/DanCornutt/session07/html_render.py
+++ students/DanCornutt/session07/html_render.py
@@ -9,23 +9,31 @@ A class-based system for rendering html.
 class Element:
     tag = "html"
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwags):
         if content:
             self.contents = [content]
         else: self.contents = []
+        self.tag_attributes = kwags
 
     def append(self, new_content):
         self.contents.append(new_content)
 
     def render(self, out_file):
+        open_tag = ["<{}".format(self.tag)]
+        # print(self.tag_attributes.keys(), self.tag_attributes.values())
+        for key, value in self.tag_attributes.items():
+            open_tag.append(' {}="{}"'.format(key, value))
+        open_tag.append(">\n")
+        out_file.write("".join(open_tag))
         for content in self.contents:
-            out_file.write("<{}>".format(self.tag))
             try:
                 content.render(out_file)
             except AttributeError:
                 out_file.write(content)
             out_file.write("\n")
         out_file.write("</{}>".format(self.tag))
+
+
 
 class Html(Element):
     tag = "html"
@@ -43,7 +51,7 @@ class OneLineTag(Element):
     def render(self, out_file):
         out_file.write("<{}>".format(self.tag))
         out_file.write(self.contents[0])
-        out_file.write("</{}>".format(self.tag))
+        out_file.write("</{}>\n".format(self.tag))
 
     def append(self, content):
         raise NotImplementedError
