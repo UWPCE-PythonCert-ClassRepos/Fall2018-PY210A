9070037ce01578c00f400a6a9a918877470dd01e
Fyfy Nguyen <fyfy@downstreamimpact.com>
2018-11-28 00:46:47 -0800

    Resolve merge conflicts

--- students/FyfyNguyen/session03/mailroom.py
+++ students/FyfyNguyen/session03/mailroom.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 
 """
@@ -102,40 +101,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-#!/usr/bin python3
-
-"""
-mailroom assignment
-"""
-
-donors = [("Fred Jones", [100, 200, 300]),
-        ("Amy Schumer", [2000, 4000, 1000])
-        ]
-
-def thank_you():
-    print("do the thank you think now")
-
-def make_report():
-    print("put report here")
-
-def main():
-    print("Welcome to Mailroom!")
-    answer = ""
-    while answer != "q":
-        print("Please select from the following")
-        print("Quit: 'q' \n"
-            "Thank you: 't' \n"
-            "Report: 'r'")
-        answer = input(" => ")
-        answer = answer.strip()
-        answer = answer[0].lower()
-        if answer == 't':
-            thank_you()
-        elif answer == 'r':
-            make_report()
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> 4ab412efe2326d06c95fd651bd03e8e5097baa89
