3a2c6987e168ef09c80af36d53c661e59c0d3f89
D Smith <dsmith82@uw.edu>
2018-11-29 22:19:35 -0800

    circle and sphere classes and tests

--- students/DrewSmith/session08/circle.py
+++ students/DrewSmith/session08/circle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 """
-Classes that represent circles
+Class that represents a circle
 """
 from math import pi
 
 class Circle():
+
+    @classmethod
+    def from_diameter(cls, diameter):
+        return cls(diameter / 2)
 
     def __init__(self, radius):
         if radius is None:
@@ -26,7 +30,77 @@ class Circle():
 
 
     def __str__(self):
-        return f"Circle with a radius: {self.radius:.6f}"
+        return f"{self.__class__.__name__} with a radius: {self.radius:.6f}"
 
     def __repr__(self):
-        return f"Circle({self.radius})"
+        return f"{self.__class__.__name__}({self.radius})"
+
+    def __add__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        return Circle(self.radius + val)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        self.radius += val
+        return self
+
+    def __sub__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        return Circle(self.radius - val)
+
+    def __rsub__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        return Circle(val - self.radius)
+
+    def __isub__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        self.radius -= val
+        return self
+
+    def __mul__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        return Circle(self.radius * val)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __imul__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        self.radius *= val
+        return self
+
+    def __truediv__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        return Circle(self.radius * val)
+
+    def __rtruediv__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        return Circle(val / self.radius)
+
+    def __itruediv__(self, other):
+        val = other if not isinstance(other, Circle) else other.radius
+        self.radius /= val
+        return self
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+
+class Sphere(Circle):
+    """
+    Represents a Sphere
+    """
+
+    @property
+    def area(self):
+        return 4 * pi * (self.radius ** 2)
+
+    @property
+    def volume(self):
+        return (4 / 3) * pi * (self.radius ** 3)
