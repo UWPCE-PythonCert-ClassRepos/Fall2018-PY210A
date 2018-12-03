68e10561fa51808a371d370f57084ea2917f3dec
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-12-02 16:36:19 -0800

    updating.

--- students/C_Farris/session07/html_render.py
+++ students/C_Farris/session07/html_render.py
@@ -2,6 +2,9 @@
 
 """
 A class-based system for rendering html.
+Stuck on kwargs. Need to review that assignment befoer I can complete...
+Problem: I am not passing the kwargs correctly. 
+
 """
 
 
@@ -9,7 +12,8 @@ A class-based system for rendering html.
 class Element(object):
     tag = 'html'
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
+        #self.kwargs = kwargs
         if not content:
             self.contents = []
         else:
@@ -19,18 +23,58 @@ class Element(object):
         self.contents.append(new_content)
 
     def render(self, out_file):
+        #if self.kwargs:
+        #    out_file.write('<{}{}>\n'.format(self.tag))
+        #else:
         out_file.write('<{}>\n'.format(self.tag))
         for content in self.contents:
-            #out_file.write(content)
-            content.render(out_file)
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
             out_file.write("\n")
         out_file.write('</{}>\n'.format(self.tag))
 
-class Html (Element):
+
+class Html(Element):
     tag = "html"
 
-class Body (Element):
+
+class Body(Element):
     tag = 'body'
 
-class P (Element):
-    tag = 'p'
+
+class P(Element):
+    tag = "p"
+
+
+class Head(Element):
+    tag = 'head'
+
+
+class OneLineTag(Element):
+    def render(self, out_file):
+        out_file.write('<{}>'.format(self.tag))
+        for content in self.contents:
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+        out_file.write('</{}>\n'.format(self.tag))
+
+class Title(OneLineTag):
+    tag = 'title'
+
+
+
+page = Html()
+head = Head()
+head.append(Title("PythonClass = Revision 1087:"))
+page.append(head)
+body = Body()
+#body.append(P("is this bold?", style="font-weight:bold"))
+body.append(P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
+body.append(P("And here is another piece of text -- you should be able to add any number"))
+page.append(body)
+with open("test.html", 'w') as outfile:
+    page.render(outfile)
