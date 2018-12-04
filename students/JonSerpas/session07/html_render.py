8ff2cd264045aa5fecc9dc6c7164405e23eac919
f3rnet <serpasj@gmail.com>
2018-12-04 12:21:27 -0800

    about as far as I can get

--- students/JonSerpas/session07/html_render.py
+++ students/JonSerpas/session07/html_render.py
@@ -10,26 +10,39 @@ class Element(object):
 
     tag = "html"
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         if content is None:
             self.contents = []
         else:
             self.contents = [content]  # created a list with content
         print("contents is :", self.contents)
+        self.attributes = kwargs
 
     def append(self, new_content):
         self.contents.append(new_content)
 
+    def _open_tag(self):
+        open_tag = [("<{}".format(self.tag))]
+        for key, value in self.attributes.items():
+            open_tag.append(' {}="{}"'.format(key, value))
+        open_tag.append(">")
+        return "".join(open_tag)
+
     def render(self, out_file):
         # loop through the list of contents:
+        # open_tag = [("<{}".format(self.tag))]
+        # for key, value in self.attributes.items():
+        #     open_tag.append(' {}="{}"'.format(key, value))
+        # open_tag.append(">\n")
+        open_tag = self._open_tag()
+        out_file.write(open_tag + '\n')
         for content in self.contents:
-            out_file.write("<{}>\n".format(self.tag))
             try:
                 content.render(out_file)
             except AttributeError:
                 out_file.write(content)
             out_file.write("\n")
-            out_file.write("</{}>\n".format(self.tag))
+        out_file.write("</{}>\n".format(self.tag))
 
 
 class Html(Element):
@@ -49,11 +62,78 @@ class Head(Element):
 
 
 class OneLineTag(Element):
-    pass
+    def render(self, out_file):
+        out_file.write("<{}>".format(self.tag))
+        out_file.write(self.contents[0])
+        out_file.write("</{}>\n".format(self.tag))
+        
+
+    def append(self, new_content):
+        raise NotImplementedError
+        self.contents.append(new_content)
 
 
 class Title(OneLineTag):
     tag = "title"
+
+
+class SelfClosingTag(Element):
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            raise TypeError("SelfClosingTag can not contain any content")
+        super().__init__(content=content, **kwargs)
+
+    def render(self, out_file):
+        # loop through the list of contents:
+        out_file.write(self._open_tag())
+        out_file.write("\n")
+        for content in self.contents:
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+                out_file.write("\n")
+        out_file.write(self._close_tag())
+        out_file.write("\n")
+
+    def append(self, *args):
+        raise TypeError("You can not add content to a SelfClosingTag")
+
+
+class Hr(SelfClosingTag):
+    tag = 'hr'
+
+    def render(self, out_file):
+        tag = self._open_tag()[:-1] + " />\n"
+        out_file.write(tag)
+
+
+class Br(SelfClosingTag):
+    tag = 'br'
+
+    def render(self, out_file):
+        tag = self._open_tag()[:-1] + " />\n"
+        out_file.write(tag)
+
+
+class A(OneLineTag):
+
+    tag = 'a'
+
+    def __init__(self, link, content=None, **kwargs):
+        kwargs['href'] = link
+        super().__init__(content, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
