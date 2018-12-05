f9a6b00d2af7ad7cbadd61932cb0f01c0c6099a4
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-12-04 19:23:51 -0800

    updating...

--- students/C_Farris/session07/html_render.py
+++ students/C_Farris/session07/html_render.py
@@ -2,9 +2,9 @@
 
 """
 A class-based system for rendering html.
-Stuck on kwargs. Need to review that assignment befoer I can complete...
-Problem: I am not passing the kwargs correctly. 
 
+On Step 5. I refactored and the code renders correctly.
+I need to add the tests to the test module before I go on to 6
 """
 
 
@@ -13,7 +13,9 @@ class Element(object):
     tag = 'html'
 
     def __init__(self, content=None, **kwargs):
-        #self.kwargs = kwargs
+
+        self.attributes = kwargs
+
         if not content:
             self.contents = []
         else:
@@ -22,18 +24,25 @@ class Element(object):
     def append(self, new_content):
         self.contents.append(new_content)
 
+    def _open_tag(self, out_file):
+        open_tag = ["<{}".format(self.tag)]
+        for key, value in self.attributes.items():
+            open_tag.append(' {}="{}"'.format(key, value))
+        out_file.write("".join(open_tag))
+
+    def _close_tag(self, out_file):
+        out_file.write('</{}>\n'.format(self.tag))
+
     def render(self, out_file):
-        #if self.kwargs:
-        #    out_file.write('<{}{}>\n'.format(self.tag))
-        #else:
-        out_file.write('<{}>\n'.format(self.tag))
+        self._open_tag(out_file)
+        out_file.write(">\n")
         for content in self.contents:
             try:
                 content.render(out_file)
             except AttributeError:
                 out_file.write(content)
             out_file.write("\n")
-        out_file.write('</{}>\n'.format(self.tag))
+        self._close_tag(out_file)
 
 
 class Html(Element):
@@ -53,26 +62,46 @@ class Head(Element):
 
 
 class OneLineTag(Element):
+
     def render(self, out_file):
-        out_file.write('<{}>'.format(self.tag))
+        self._open_tag(out_file)
+        out_file.write(">")
         for content in self.contents:
             try:
                 content.render(out_file)
             except AttributeError:
                 out_file.write(content)
-        out_file.write('</{}>\n'.format(self.tag))
+        self._close_tag(out_file)
+
 
 class Title(OneLineTag):
     tag = 'title'
 
 
+class SelfClosingTag(Element):
+
+    def render(self, out_file):
+        self._open_tag(out_file)
+        out_file.write("/>")
+
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+
+
+class Br(SelfClosingTag):
+    tag = "br"
+
+
 
 page = Html()
 head = Head()
-head.append(Title("PythonClass = Revision 1087:"))
+head.append(Title("PythonClass = Revision 1087:" , style="font-weight:bold"))
 page.append(head)
 body = Body()
-#body.append(P("is this bold?", style="font-weight:bold"))
+body.append(Hr(style="font-weight:bold", id="intro"))
+body.append(Br(style="font-weight:bold", id="intro"))
+body.append(P("is this bold?", style="font-weight:bold", id="intro"))
 body.append(P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
 body.append(P("And here is another piece of text -- you should be able to add any number"))
 page.append(body)
