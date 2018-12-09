e6db616bc10bbbb4b1d1cc699e9b4bab1bf4ec7f
Jim Jenkins <dvlupr@uw.edu>
2018-12-09 08:43:44 -0800

    general clean-up.

--- students/JimJenkins/session08/circle.py
+++ students/JimJenkins/session08/circle.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 
+class Circle():
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property
+    def diameter(self):
+        return 2 * self.radius
+
+
+c = Circle(5)
+print(c.radius)
+
+c.radius = 7
+print(c.diameter)
