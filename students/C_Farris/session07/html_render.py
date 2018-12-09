fcef9790ffe27d6882bed3dd56c31cb4bd223a9c
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-12-09 04:26:21 -0800

    updating html_render.py. Almost finished!

--- students/C_Farris/session07/html_render.py
+++ students/C_Farris/session07/html_render.py
@@ -3,17 +3,16 @@
 """
 A class-based system for rendering html.
 
-On Step 5. I refactored and the code renders correctly.
-I need to add the tests to the test module before I go on to 6
+Completed Step 5 and on to step 6. Haven't begun
 """
 
 
 # This is the framework for the base class
 class Element(object):
     tag = 'html'
-
+    
     def __init__(self, content=None, **kwargs):
-
+        self.indent = ""
         self.attributes = kwargs
 
         if not content:
@@ -21,10 +20,15 @@ class Element(object):
         else:
             self.contents = [content]
 
+    def addIndent(self):
+        self.indent += "    " 
+
     def append(self, new_content):
         self.contents.append(new_content)
 
     def _open_tag(self, out_file):
+        if self.tag == 'html':
+            out_file.write('<!DOCTYPE html>\n')
         open_tag = ["<{}".format(self.tag)]
         for key, value in self.attributes.items():
             open_tag.append(' {}="{}"'.format(key, value))
@@ -33,16 +37,19 @@ class Element(object):
     def _close_tag(self, out_file):
         out_file.write('</{}>\n'.format(self.tag))
 
-    def render(self, out_file):
-        self._open_tag(out_file)
-        out_file.write(">\n")
-        for content in self.contents:
-            try:
-                content.render(out_file)
-            except AttributeError:
-                out_file.write(content)
-            out_file.write("\n")
-        self._close_tag(out_file)
+    def render(self, out_file, cur_ind=""):
+        if self.attributes is None:
+            out_file.write(self._open_tag())
+        else:    
+            self._open_tag(out_file)
+            out_file.write(">\n")
+            for content in self.contents:
+                try:
+                    content.render(out_file)
+                except AttributeError:
+                    out_file.write(content)
+                out_file.write("\n")
+            self._close_tag(out_file)
 
 
 class Html(Element):
@@ -63,7 +70,7 @@ class Head(Element):
 
 class OneLineTag(Element):
 
-    def render(self, out_file):
+    def render(self, out_file, cur_ind=""):
         self._open_tag(out_file)
         out_file.write(">")
         for content in self.contents:
@@ -80,9 +87,21 @@ class Title(OneLineTag):
 
 class SelfClosingTag(Element):
 
-    def render(self, out_file):
-        self._open_tag(out_file)
-        out_file.write("/>")
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            raise TypeError("SelfClosingTag can not contain any content")
+        super().__init__(content=content, **kwargs)
+
+    def render(self, out_file, cur_ind=""):
+        tag = self._open_tag(out_file)
+        out_file.write("/>\n")
+
+    def append(self, *args):
+        raise TypeError("you can\'t append content to a self closing tag")
+
+
+class Meta(SelfClosingTag):
+    tag = 'meta'
 
 
 class Hr(SelfClosingTag):
@@ -93,14 +112,40 @@ class Br(SelfClosingTag):
     tag = "br"
 
 
+class A(OneLineTag):
+    tag = 'a'
+
+    def __init__(self, link, content=None, **kwargs):
+        kwargs['href'] = link 
+        super().__init__(content, **kwargs)
+
+class UL(Element):
+    tag = 'ul'
+
+
+class Li(Element):
+    tag = 'li'
+
+class H(OneLineTag):
+    """Header Element"""
+    def __init__(self, H_level, content=None, **kwargs):
+        self.tag = H_level
+        self.content = [content]
+        super().__init__(content, **kwargs)
+
+
+
 
 page = Html()
 head = Head()
 head.append(Title("PythonClass = Revision 1087:" , style="font-weight:bold"))
 page.append(head)
 body = Body()
+body.append(H('h1','hello'))
 body.append(Hr(style="font-weight:bold", id="intro"))
 body.append(Br(style="font-weight:bold", id="intro"))
+body.append(UL(id="intro"))
+body.append(Li("peace be with you", style="font-weight:bold"))
 body.append(P("is this bold?", style="font-weight:bold", id="intro"))
 body.append(P("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
 body.append(P("And here is another piece of text -- you should be able to add any number"))
