5ac34ae65f4cbf0bb360dd7a6ebbd44a4f33da5b
Dianna Tingg <dtingg@uw.edu>
2018-12-09 19:21:24 -0800

    updated donor_models

--- students/DiannaTingg/Session09/mailroom_oo/donor_models.py
+++ students/DiannaTingg/Session09/mailroom_oo/donor_models.py
@@ -1,18 +1,16 @@
 # Lesson 09 Assignment: Mailroom - Object Oriented
 # Donor and DonorCollection Classes
 
+import datetime
+
 
 class Donor:
     """
-    This class holds all the info about a single donor
+    This class holds all the info about a single donor.
     """
-    def __init__(self, name, initial_donation=None):
+    def __init__(self, name):
         self.name = name
-
-        if initial_donation is None:
-            self.donations = []
-        else:
-            self.donations = [initial_donation]
+        self.donations = []
 
     def add_donation(self, new_donation):
         self.donations.append(new_donation)
@@ -27,47 +25,52 @@ class Donor:
 
     @property
     def avg_donation(self):
-        return self.total_donations/self.num_donations
+        return self.total_donations / self.num_donations
+
+    def thank_you_letter(self):
+        date = datetime.datetime.now().strftime("%B %d, %Y")
+        letter = f"{date}\n\n"\
+            f"Dear {self.name}:\n\n"\
+            f"Thank you so much for the generous donation of ${self.donations[-1]:,.2f}.\n"\
+            f"We will use the money to help Earthlings move to Mars!\n\n"\
+            f"Best regards,\nDianna Tingg\nMars Foundation"
+        return letter
 
 
-
-# This class holds all of the donor objects as well as methods to add a new donor, search for a donor, etc
-# Has a method to save/reload data
-# Generates reports about all donors
 class DonorCollection:
     """
-    This class holds all of the donor objects
+    This class holds all of the donor objects and methods to search for a donor, add a new donor, save/reload data,
+    run a report, etc.
     """
     def __init__(self):
         self.donors_dict = {}
 
-    def create_donor(self, donor_name, initial_donation=None):
-        new_donor = Donor(donor_name, initial_donation)
+    def create_donor(self, donor_name):
+        new_donor = Donor(donor_name)
         self.donors_dict[donor_name] = new_donor
         return new_donor
 
-    def find_donor(self, donor_name, donation):
+    def find_donor(self, donor_name):
         try:
             return self.donors_dict[donor_name]
         except KeyError:
-            return self.create_donor(donor_name, donation)
+            return self.create_donor(donor_name)
 
     def list_donors(self):
         donor_list = [donor.name for donor in self.donors_dict.values()]
         sorted_donors = sorted(donor_list)
         return sorted_donors
 
-    # Create sort key to return total donated
     @staticmethod
+    # Returns total donations
     def sort_key(donor_stats):
         return donor_stats[1]
 
-    # Create a report of all donors
+    # Sorts donors by total donations, in descending order
     def create_report(self):
         summary = [[donor.name, donor.total_donations, donor.num_donations, donor.avg_donation]
                    for donor in self.donors_dict.values()]
 
-        # Sort Summary by total given in descending order
         sorted_summary = sorted(summary, key=self.sort_key, reverse=True)
 
         return sorted_summary
