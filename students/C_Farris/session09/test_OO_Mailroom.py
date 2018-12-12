69e02a3998ec75c97d37ac5d2dc325d76602c6fc
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-12-11 18:36:40 -0800

    Updating test_OO_Mailroom WIP

--- students/C_Farris/session09/test_OO_Mailroom.py
+++ students/C_Farris/session09/test_OO_Mailroom.py
@@ -37,39 +37,66 @@ def test_donor_collection():
 
 
 def test_add_donor():
+    """Tests that you can add a donor to the DonorCollection object. 
+       Then add a donation, then confirm the name is they key in the
+       dictionary."""
     donor = Donor("Pippy Longstocking")
     dc = DonorCollection()
-    print(dc.donor_dict)
     donor.add_donation(25)
-    print("donor name", donor.name)
-    print("donor donations", donor.donations)
-
     dc.addDonor(donor.name, donor)
-    print(dc.donor_dict[donor.name])
-   
     donor2 = Donor("Manu Chow")
     donor2.add_donation(500)
     dc.addDonor(donor2.name, donor2)
-    print(dc.donor_dict[donor2.name])
-    print(dc.donor_dict)
 
-    assert  2 == 25
+    assert "Manu Chow" in dc.donor_dict.keys()
+    assert "Pippy Longstocking" in dc.donor_dict.keys()
+   
 
-def test_get_donor_Object():
-    pass    
+def test_get_sum_of_donations():
+    donor = Donor("Pippy Longstocking")
+    donor.add_donation(25)
+    donor.add_donation(300)
+    donor.add_donation(500)
+
+    assert donor.sum_donations() == 825
 
 
+def test_get_last_donation():
+    donor = Donor("Pippy Longstocking")
+    donor.add_donation(25)
+    donor.add_donation(300)
+    donor.add_donation(500)
+    print(donor.get_last_donation())
+    assert donor.get_last_donation() == 500
 
-#def test_get_last_donation():
-#    pass
+def test_sum_thank_your_letter():
+    """Returns a thank you letter that defaults to last value unless you indicate sum for the thank you to reference sum"""
+    donor = Donor("Pippy Longstocking")
+    donor.add_donation(25)
+    donor.add_donation(300)
+    donor.add_donation(500)
+
+    print(donor.thank_your_letter())
+    assert donor.thank_your_letter() == dedent(
+        '''\tDear Pippy Longstocking,
+        Thank you for your generous donation of $500 to our cause.
+        
+        Sincerely,
+        The Team''')
+
+    print(donor.thank_your_letter('sum'))
+    assert donor.thank_your_letter('sum') == dedent(
+        '''\tDear Pippy Longstocking,
+        Thank you for your generous donation of $825 to our cause.
+        
+        Sincerely,
+        The Team''')
 
 
-#def test_get_sum_of_donations():
-#    pass
+#def test_get_donor_Object():
+
 
 
 #def test_search_donor():
 #    pass
 
-#def test_donor_thank_your_letter():
-#    pass
