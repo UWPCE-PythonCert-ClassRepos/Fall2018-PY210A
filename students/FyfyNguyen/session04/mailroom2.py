57fe3da3a286ddb8dea9bec1d4ec4e685382ad16
Fyfy Nguyen <fyfy@downstreamimpact.com>
2018-12-12 03:10:34 -0800

    Add mailroom 2 refactor. Create report needs work still.

--- students/FyfyNguyen/session04/mailroom2.py
+++ students/FyfyNguyen/session04/mailroom2.py
@@ -3,7 +3,7 @@
 """
 Mailroom 2
 """
-import os
+
 import sys
 import tempfile
 import pathlib
@@ -20,32 +20,56 @@ file_dir = pathlib.Path(tempfile.gettempdir())
 
 
 def print_donors():
+    """
+    Prints a list of exiting donors in donor_db
+    """
     for donor in donor_db:
         print(donor)
 
 
 def find_donor(name_entered):
+    """
+    Lookup donor in donor_db
+
+    :param: the name of the donor
+    :returns: the donor data structure -- None if donor not found in donor_db
+    """
     donor_key = name_entered.strip().title()
     return donor_db.get(donor_key)
 
 
 def add_donor(name_entered):
+    """
+    Add a new donor to donor_db
+
+    :param: the name of the donor
+    :returns: the new donor data structure
+    """
     donor = (name_entered.title(), [])
-    donor_db[name_entered.title()] = donor
+    donor_db[name_entered] = donor
     print("Successfully added new donor to database.")
     add_donation(name_entered)
-    return donor
+
 
 def add_donation(name_entered):
+    """
+    Ask the user for donation amount and add it to donor_db
+
+    :param: the name of the donor
+    """
     donation = float(input("Enter donation amount >>> "))
-    donor = (name_entered.title(), [])
+    donor = (name_entered, [])
     donor[1].append(donation)
     print("Successfully updated donation amount.")
-
     send_thank_you(name_entered, donation)
 
 
 def confirm_donor(name_entered):
+    """
+    Confirm the user wants to add a new donor to donor_db
+
+    :param: the name of the donor
+    """
     response = input(f"Donor does not exist. Add {name_entered.title()} to "
                      "database? [y/n?] >>> ")
     if response == 'y':
@@ -55,17 +79,26 @@ def confirm_donor(name_entered):
 
 
 def send_thank_you(name_entered, donation):
-    print(dedent("""
-        Dear {},
+    """
+    Generate a thank you letter to the donor
+
+    :param: the name of the donor
+    :param: the donation amount
+    :returns:string with letter
+    """
+    return dedent("""Dear {},
 
             Your generous ${:,.2f} donation just made our day!
 
                 Thank you!
-                -The Charity""".format(name_entered.title(), donation)
-                 ))
+                -Our Charity
+        """.format(name_entered.title(), donation))
 
 
 def thank_you():
+    """
+    Execute the logic to record a donation and generate a thank you message
+    """
     while True:
         name_entered = input("Enter the name of a donor or 'list' to view "
                              "donors >>> ").lower().strip()
@@ -77,30 +110,50 @@ def thank_you():
     donor = find_donor(name_entered)
     if donor is None:
         confirm_donor(name_entered)
-    else:
-        get_donor(name_entered)
 
 
 def thank_you_all():
-    pass
+    """
+    Generate a letter for each donor and saves it to temporary directory
+    """
+    for donor in donor_db:
+        letters = send_thank_you(donor, donor_db[donor][-1])
+        file_path = file_dir / (donor.replace(" ", "_") + ".txt")
+        with open(file_path, 'w') as f:
+            f.write(letters)
+    print(f'Files downloaded to: {file_dir}')
 
 
-def sort_key(donors):
+def sort_key(donor):
     return donor_db[1]
 
 
 def create_report():
-    print("{:30s} | {:11s} | {:9s} | {:12s}".format(
-          "Donor Name", "Total Given", "Num Gifts", "Average Gift"))
+    """
+    Generate report of donors and the amounts donated
+
+    :returns: the donor report as a string
+    """
+    print("{:30s} | {:11s} | {:9s} | {:12s}".format("Donor Name",
+                                                    "Total Given",
+                                                    "Num Gifts",
+                                                    "Average Gift"
+                                                    ))
     print("-" * 72)
-    donors.sort(key=sort_key, reverse=True)
-    for donor in donors:
-        print("{:30s}  $ {:11,.2f}  {:9d}  $ {:12,.2f}".format(
-              donor[0], sum(donor[1]), len(donor[1]),
-              sum(donor[1]) / len(donor[1])))
+    donor_db.sort(key=sort_key, reverse=True)
+    for donor in donor_db:
+        print("{:30s}  $ {:11,.2f}  {:9d}  $ {:12,.2f}".format(donor[0],
+                                                               sum(donor[1]),
+                                                               len(donor[1]),
+                                                               sum(donor[1]) /
+                                                               len(donor[1])
+                                                               ))
 
 
 def exit_program():
+    """
+    Quit the program
+    """
     print("Exiting Program. Goodbye!")
     sys.exit()
 
