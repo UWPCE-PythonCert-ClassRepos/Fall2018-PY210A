a7a6b604fc36492fa5796aadd5ef8534599a535d
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-12-12 23:03:27 -0800

    updating OO mailroom test WIP

--- students/C_Farris/session09/test_OO_Mailroom.py
+++ students/C_Farris/session09/test_OO_Mailroom.py
@@ -6,10 +6,24 @@
 #Name: test_OO_Mailroom.Python.py
 #Purpose: Create an object oriented Mailroom
 #Progress:
-#Working on getting the tests up and running
+#Mostly done with Donor class. Now fill out and test Donor Collection
 #####
 
 from OO_Mailroom import *
+# .donor_Models for the donors in test_donor_models
+
+#for CLI
+#from donor_models import Donor, DonorCollection, any other...
+
+
+donor_mock2 = [Donor("William Gates III", [653772.32, 12.17]),
+              Donor("Jeff Bezos", [877.33]),
+              Donor("Paul Allen", [663.23, 43.87, 1.32]),
+              Donor("Mark Zuckerberg", [1663.23, 4300.87, 10432.0]),
+              Donor("John Galt", [25.00, 9038.01, 0.01])]                        
+
+
+#######################Test Donor Class################
 
 
 def test_donor_name():
@@ -19,6 +33,7 @@ def test_donor_name():
 
 
 def test_add_donation():
+    """Tests you can add a donation from multiple donors"""
     donor = Donor("Yo Mama")
     donor.add_donation(500)
     donor.add_donation(200)
@@ -26,31 +41,18 @@ def test_add_donation():
     assert donor.donations[0] == 500
     assert donor.num_donations == 2
 
-"""
-Test, donation is numerical to add donation
-"""
 
+def test_add_donation_checks():
+    """tests that add_donation cheks donation values are numeric and greater than a penny"""
+    donor = Donor("Yo Mama")
+    donor.add_donation(200)
+    donor.add_donation(500)
+    
+    assert donor.add_donation("apple") == "Donation value must be entirely numeric!"
+    assert donor.add_donation(0.0003) == "Donation cannot be smaller than a penny!"
+    assert donor.donations[1] == 500
+    assert donor.num_donations == 2
 
-def test_donor_collection():
-    dc = DonorCollection()
-
-
-
-def test_add_donor():
-    """Tests that you can add a donor to the DonorCollection object. 
-       Then add a donation, then confirm the name is they key in the
-       dictionary."""
-    donor = Donor("Pippy Longstocking")
-    dc = DonorCollection()
-    donor.add_donation(25)
-    dc.addDonor(donor.name, donor)
-    donor2 = Donor("Manu Chow")
-    donor2.add_donation(500)
-    dc.addDonor(donor2.name, donor2)
-
-    assert "Manu Chow" in dc.donor_dict.keys()
-    assert "Pippy Longstocking" in dc.donor_dict.keys()
-   
 
 def test_get_sum_of_donations():
     donor = Donor("Pippy Longstocking")
@@ -62,11 +64,11 @@ def test_get_sum_of_donations():
 
 
 def test_get_last_donation():
-    donor = Donor("Pippy Longstocking")
+    donor = Donor("Pippy Longstocking", 10)
     donor.add_donation(25)
     donor.add_donation(300)
     donor.add_donation(500)
-    print(donor.get_last_donation())
+
     assert donor.get_last_donation() == 500
 
 def test_sum_thank_your_letter():
@@ -92,11 +94,41 @@ def test_sum_thank_your_letter():
         Sincerely,
         The Team''')
 
+def test_repr_():
+    donor = Donor("Pippy Longstocking")
+    donor.add_donation(25)
+    donor.add_donation(300)
+    print(donor)
+    assert repr(donor) == 'Donor(Pippy Longstocking) : [25, 300]'
 
-#def test_get_donor_Object():
 
 
+#######################Test Donor Collection Class################
 
-#def test_search_donor():
-#    pass
+def test_donor_collection():
+    """tests to see I can instantiate a donor collection and that I am able to add a list of donors"""
+    dc = DonorCollection()
+    dp = DonorCollection(donor_mock2)
+
+    assert "John Galt" in dp.donor_dict.keys()
+
+
+def test_add_donor():
+    """Tests that you can add a donor to the DonorCollection object. 
+       Then add a donation, then confirm the name is they key in the
+       dictionary."""
+    donor = Donor("Pippy Longstocking")
+    dc = DonorCollection()
+    donor.add_donation(25)
+    dc.addDonor(donor.name, donor)
+    donor2 = Donor("Manu Chow")
+    donor2.add_donation(500)
+    dc.addDonor(donor2.name, donor2)
+
+    assert "Manu Chow" in dc.donor_dict.keys()
+    assert "Pippy Longstocking" in dc.donor_dict.keys()
+
+
+def test_search_donor():
+    pass
 
