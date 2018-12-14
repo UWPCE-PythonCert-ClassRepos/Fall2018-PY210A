f51d51d7b5c288782ca5a6e778ee80f2f7e21aa7
Ronald F Tallman <framllat@gmail.com>
2018-12-14 06:14:18 -0800

    going thru Solution database and refreshing concepts Ron Tallman

---
+++ students/student_framllat/session02/print_grid_rft.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+Chris' solution to the grid printing Exercise.
+
+Note that we only did the basics of loops, and you can
+do all this without any loops at all, so that's what I did.
+
+Note also that there is more than one way to skin a cat --
+   or code a function
+"""
+
+# NOTE -- Going thru lessons as a well needed refresh using Solution Code!
+# by Ron Tallman
+
+
+def print_grid_ez():
+    """
+    Did anyone come up with the most trivial possible solution?
+    """
+    print("""
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+""")
+
+print_grid_ez()
