4d9fa000f45260de9f211f60e1f5231ca7114b68
Ronald F Tallman <framllat@gmail.com>
2018-12-14 11:32:04 -0800

    some more mods of lesson02

--- students/student_framllat/session02/print_grid_rft.py
+++ students/student_framllat/session02/print_grid_rft.py
@@ -18,6 +18,7 @@ def print_grid_ez():
     """
     Did anyone come up with the most trivial possible solution?
     """
+    print("\nPrinting out the box using comments")
     print("""
 + - - - - + - - - - +
 |         |         |
@@ -32,4 +33,34 @@ def print_grid_ez():
 + - - - - + - - - - +
 """)
 
+def print_grid(size):
+    """
+    print a 2x2 grid with a total size of size
+
+    :param size: total size of grid -- it will be rounded if not one more than
+        a multiple of 2
+    """
+    number = 2
+    box_size = int((size - 1) // 2)  # size of one grid box: integer division
+
+    print("\t\t\tOur first non-EZ Box")
+    print("The Box Size will be: \t\t", box_size)
+    # top row
+    top = ('+ ' + '- ' * box_size) * number + '+' + '\n'
+    middle = ('| ' + ' ' * 2 * box_size) * number + '|' + '\n'
+
+    row = top + middle * box_size
+    print("The Top will look like: \t", top.strip())
+    print("The Middle will look like: \t", middle)
+
+    grid = row * number + top
+
+    print(grid)
+
+################
+# Function calls
+################
+
 print_grid_ez()
+
+print_grid(7)
