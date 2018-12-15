5c5f36082b97052fc85f0ef6fa49a5cba7081cc0
Cheng Liu <chengliumba@gmail.com>
2018-12-14 17:33:47 -0800

    updated mailroom_OO (WIP 12-14-2018)

--- students/ChengLiu/session09/mailroom_final.py
+++ students/ChengLiu/session09/mailroom_final.py
@@ -5,15 +5,15 @@ Cheng Liu
 Mail-room Final
 """
 
-import sys
+# import sys
 import math
 
 
-def default_donor():
-    """Testing data"""
-    return [Donor("Default Test1", [1, 2, 3]),
-            Donor("Default Test2", [10, 20, 30]),
-            ]
+# def default_donor():
+#     """Testing data"""
+#     return [Donor("Default Test1", [1, 2, 3]),
+#             Donor("Default Test2", [10, 20, 30]),  # tuples to list
+#             ]
 
 
 class Donor():
@@ -24,11 +24,18 @@ class Donor():
         if donations is None:
             self.donations = []
         else:
-            self.donations = list(donations)
+            self.donations = list(donations)  # converting donations from tuple to list
 
     @property
-    def donor(self):
-        return self.donors.values()
+    def donor_name(self):
+        return self.name
+
+    @property
+    def list_donations(self):
+        try:
+            return self.donations
+        except IndexError:
+            return None
 
     @property
     def last_donation(self):
@@ -39,53 +46,84 @@ class Donor():
 
     @property
     def total_donations(self):
-        return sum(self.donations)
+        try:
+            return sum(self.donations)
+        except IndexError:
+            return 0
 
     @property
     def num_of_donations(self):
-        return len(self.donations)
+        try:
+            return len(self.donations)
+        except IndexError:
+            return 0
 
     @property
     def avg_donations(self):
-        return self.total_donations / self.num_of_donations
+        try:
+            return self.total_donations / self.num_of_donations
+        except ZeroDivisionError:
+            return None
 
-    def add_donation(self, donation):
-        """ add new donations"""
-        if donation <= 0.0:
-            raise ValueError("Donation amount must be greater than zero!")
-        self.donations.append(donation)
 
-    def list_donors(self):
-        current_donors = []
-        for donor in self.donors:
-            current_donors.append(donor.name)
-        return "\n".join(current_donors)
+class Donor_Library():
+    def __init__(self, donors=None):
+        if donors is None:
+            self.donor_lib = {}
+        else:
+            self.donor_lib = {d.name: d.donations for d in donors}
 
-    def find_donor(self, name):
-        return self.donors.get(Donor(name))
+    @property
+    def find_donors(self, name):
+        if self.key() == name:
+            return {self.key(), self.values()}
 
-    def add_donor(self, name):
-        new_donor = Donor(name)
-        self.donors[name] = new_donor
+    # def find_donor(self, name):
+    #     if self.name == name:
+    #         return Donor[name]
+    #     return self.get(name())
 
-    def letter(self, donor):
-        return ("\nHello {}! Thank you for your donation of ${:.2}.".format(donor.name, str(donor.last_donation)) + "\n")
 
-    def report(self):
-        report_rows = []
-        for donor in self.donors:
-            report_rows.append((donor.name, donor.total_donations, donor.num_of_donations, donor.avg_donations))
 
-        report = []
-        report.append("{:<25s} | {:>11s} | {:>9s} | {:>12s}".format("Donor Name", "Total Given", "Num Gifts", "Average Gift"))
-        report.append("-" * 70)
-        for row in report_rows:
-            report.append("{:<25s} | {:>11s} | {:>9s} | {:>12s}".format(*row))
-        return "\n".join(report)
 
-    def send_letter_to_file(self):
-        for donor in self.donors.values():
-            print("Saving letter to:", donor.name)
-            saved_letters = self.letter(donor)
-            filename = "Letter_to_".join(donor.name) + ".txt"
-            open(filename, 'w').write(saved_letters)
+
+    # def add_donation(self, donation):
+    #     """ add new donations"""
+    #     if donation <= 0.0:
+    #         raise ValueError("Donation amount must be greater than zero!")
+    #     self.donations.append(donation)
+
+    # def list_donors(self):
+    #     current_donors = []
+    #     for donor in self.donors:
+    #         current_donors.append(donor.name)
+    #     return "\n".join(current_donors)
+
+    # def find_donor(self, name):
+    #     return self.donors.get(Donor(name))
+
+    # def add_donor(self, name):
+    #     new_donor = Donor(name)
+    #     self.donors[name] = new_donor
+
+    # def letter(self, donor):
+    #     return ("\nHello {}! Thank you for your donation of ${:.2}.".format(donor.name, str(donor.last_donation)) + "\n")
+
+    # def report(self):
+    #     report_rows = []
+    #     for donor in self.donors:
+    #         report_rows.append((donor.name, donor.total_donations, donor.num_of_donations, donor.avg_donations))
+
+    #     report = []
+    #     report.append("{:<25s} | {:>11s} | {:>9s} | {:>12s}".format("Donor Name", "Total Given", "Num Gifts", "Average Gift"))
+    #     report.append("-" * 70)
+    #     for row in report_rows:
+    #         report.append("{:<25s} | {:>11s} | {:>9s} | {:>12s}".format(*row))
+    #     return "\n".join(report)
+
+    # def send_letter_to_file(self):
+    #     for donor in self.donors.values():
+    #         print("Saving letter to:", donor.name)
+    #         saved_letters = self.letter(donor)
+    #         filename = "Letter_to_".join(donor.name) + ".txt"
+    #         open(filename, 'w').write(saved_letters)
--- students/ChengLiu/session09/mailroom_final_test.py
+++ students/ChengLiu/session09/mailroom_final_test.py
@@ -5,43 +5,99 @@ Cheng Liu
 Mail-room Final
 """
 
-import os
-import pytest
-import mailroom_final
+# import os
+# import pytest
+from .mailroom_final import Donor
 
 
-def test_default_donor():
-    assert donors['Test Default'] == ("Test Default", [1, 2, 3])
+def test_donor_with_donation():
+    donor_test = Donor("test name", [1, 2, 3])
+    # assert donor_test.name == "Test Name"
+    # assert donor_test.donations == [1, 2, 3]
+    assert donor_test.donor_name == "Test Name"
+    assert donor_test.list_donations == [1, 2, 3]
+    assert donor_test.last_donation == 3
 
 
-def test_add_donor():
-    donor = Donor("Kobe Br")
-    assert donor.name == "Kobe Br"
-    assert donor.last_donation is None
+def test_donor_without_donation():
+    donor_test = Donor("test name")
+    # assert donor_test.name == "Test Name"
+    # assert donor_test.donations == [1, 2, 3]
+    assert donor_test.donor_name == "Test Name"
+    assert donor_test.list_donations == []
+    assert donor_test.last_donation is None
 
 
-def test_add_donation():
-    donor.add_donation(100)
+def test_total_donations():
+    donor_test_1 = Donor("test name", [1, 2, 3])
+    assert donor_test_1.total_donations == 6
+
+    donor_test_2 = Donor("test name")
+    assert donor_test_2.total_donations == 0
 
 
-    assert donor.last_donation == 100
+def test_num_of_donations():
+    donor_test_1 = Donor("test name", [1, 2, 3])
+    donor_test_2 = Donor("test name")
+
+    assert donor_test_1.num_of_donations == 3
+    assert donor_test_2.num_of_donations == 0
 
 
-def test_list_donors():
-    assert "Default Test1" in Donor.list_donors()
+def test_avg_donations():
+    donor_test_1 = Donor("test name", [1, 2, 3])
+    donor_test_2 = Donor("test name")
+
+    assert donor_test_1.avg_donations == 2
+    assert donor_test_2.avg_donations is None
 
 
-def test_find_donors():
-    test_donor = Donor.find_donors('default test3')
-    assert test_donor is None
+# def test_find_donor():
+#     donors_test = Donor("test One", [1, 2, 3])
+
+#     donor_target = donors_test.find_donor("test one")
+#     assert donor_target.name == "Test Name"
+#     assert donor_target.donations == [1, 2, 3]
+
+#     donor_none = donors_test.find_donor("test five")
+#     assert donor_none.name is None
+#     assert donor_none.donations is None
 
 
-def test_letter():
-    donor = Donor("Jeff Bzo", [100.10, 50.50])
-    donor_letter = donor.letter(donor)
-    assert "Jeff Bzo" in donor_letter
-    assert "$50.50" in donor_letter
 
 
-def test_report():
-    assert "Jeff Bzo" in report
+
+
+
+
+# def test_add_donor():
+#     donor = Donor("Kobe Br")
+#     assert donor.name == "Kobe Br"
+#     assert donor.last_donation is None
+
+
+# def test_add_donation():
+#     donor.add_donation(100)
+
+
+#     assert donor.last_donation == 100
+
+
+# def test_list_donors():
+#     assert "Default Test1" in Donor.list_donors()
+
+
+# def test_find_donors():
+#     test_donor = Donor.find_donors('default test3')
+#     assert test_donor is None
+
+
+# def test_letter():
+#     donor = Donor("Jeff Bzo", [100.10, 50.50])
+#     donor_letter = donor.letter(donor)
+#     assert "Jeff Bzo" in donor_letter
+#     assert "$50.50" in donor_letter
+
+
+# def test_report():
+#     assert "Jeff Bzo" in report
